65dc073e4a8fa644b1ad54eb2bbaa9a98a271c24
qinking126 <myw714@gmail.com>
2013-11-26 09:38:19 -0500

    change the directory color in .bashrc file

--- provision.sh
+++ provision.sh
@@ -10,6 +10,9 @@ sudo dpkg -i puppetlabs-release-precise.deb -y >/dev/null 2>&1
 apt-get update >/dev/null 2>&1
 sudo apt-get -y install puppet >/dev/null 2>&1
 
+echo "LS_COLORS='di=1;33' ; export LS_COLORS" >> ~/.bashrc 
+
+
 touch /var/vagrant_provision
 echo "------------------------------------------------"
 echo "Installation is done"
